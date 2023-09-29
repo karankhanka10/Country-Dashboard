4d52fe9baee4ae84b0d6fb7dd18931adfa4e2f74
HarshDex <nature4917@gmail.com>
2023-09-30 02:53:38 +0530

    added homepage row 1 row 2

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -6,17 +6,49 @@ const Login = () => {
     <>
     
        <section className="bg_img">
+<<<<<<< HEAD
         <h1 className="bg_header">Welcome!</h1>
+=======
+        <h1 class="bg_header">
+          Welcome!
+          <p>Use these awesome forms to login or create new account in your project for free.
+</p>
+        </h1>
+>>>>>>> ccd47b2e7e48a82a003a5872a46c551123f61467
         <div className="card">
           <h4 id='card_header'>
               Register with
           </h4>
-            <div id="card_Fname">
+            <div id="card_Fname" placeholder='Enter your Full Name'>
 
             </div>
             <h4 id='or'>
               or
             </h4>
+          <div className="card-details-section">
+            <div className = "name dets">
+              <label htmlFor="#name">Name</label>
+              <input type="" className="inputs" id = "name"/>
+            </div>
+            <div className="email dets">
+              <label htmlFor="#email">Email</label>
+              <input type="mail" className="inputs" id = "email"/>
+            </div>
+            <div className="password dets">
+              <label htmlFor=".password">Password</label>
+              <input type="password" className="inputs" id = "password"/>
+            </div>
+            <div className="remember-me-section">
+
+            </div>
+            <div className="login-buttons">
+              <button>Sign Up</button>
+              <p>
+                already have an account? 
+                <span>Sign In</span>
+              </p>
+            </div>
+          </div>
         </div>
        </section>
        
